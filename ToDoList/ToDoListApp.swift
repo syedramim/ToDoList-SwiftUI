f140b832fc34e5b40d19d61d802e7fc054d50d78
Kaneis Zontanos <ramim@bc.edu>
2023-02-23 14:54:29 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Kaneis Zontanos on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
